4994beac02821e8e05294e0051effe210814d113
Captain-Kirk83 <vinit.kumar.met19@itbhu.ac.in>
2022-05-18 17:32:47 +0530

    Program 1 of coding prep

--- first.cpp
+++ first.cpp
@@ -1,7 +1,9 @@
 #include<bits/stdc++.h>
 
+using namespace std;
+
 int main()
 {
-    cout<<"This is the first time for github.dev";
+    cout<<"This is the first time for github.dev"<<endl;
     return 0;
 }
